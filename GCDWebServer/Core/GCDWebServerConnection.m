15430f329d825dddb060664b33535f6366909cc7
Sergio Padrino <sergio.padrino@gmail.com>
2015-03-20 13:39:58 +0100

    Add remote and local addresses to GCDWebServerRequest

--- GCDWebServer/Core/GCDWebServerConnection.m
+++ GCDWebServer/Core/GCDWebServerConnection.m
@@ -548,6 +548,8 @@ static inline NSUInteger _ScanHexNumber(const void* bytes, NSUInteger size) {
           }
         }
         if (_request) {
+          _request.localAddressData = [self.localAddressData copy];
+          _request.remoteAddressData = [self.remoteAddressData copy];
           if ([_request hasBody]) {
             [_request prepareForWriting];
             if (_request.usesChunkedTransferEncoding || (extraData.length <= _request.contentLength)) {
